266a04bf897025fc4496a01b022524052e6708f2
Balazs Ando <elnesia@gmail.com>
2016-09-02 17:13:35 +0200

    Simplify code

--- main.py
+++ main.py
@@ -4,9 +4,9 @@ from flask import Flask, g, redirect, request, session, url_for, abort, render_t
 from functools import wraps
 from peewee import *
 
-DATABASE = 'homework.db'
+DATABASE = "homework.db"
 DEBUG = True
-SECRET_KEY = 'sumthineffinrandomshiet'
+SECRET_KEY = "sumthineffinrandomshiet"
 app = Flask(__name__)
 app.config.from_object(__name__)
 database = SqliteDatabase(DATABASE)
@@ -44,49 +44,47 @@ def after_request(response):
     return response
 
 
-@app.route('/')
+@app.route("/")
 def homepage():
     return userstory_list()
 
 
-def object_list(template_name, qr, var_name='object_list', **kwargs):
-    kwargs.update(page=int(request.args.get('page', 1)), pages=(qr.count()-1) // 5 + 1)
-    kwargs[var_name] = qr.paginate(kwargs['page'], 5)
+def object_list(template_name, qr, var_name="object_list", **kwargs):
+    kwargs.update(page=int(request.args.get("page", 1)), pages=(qr.count()-1) // 5 + 1)
+    kwargs[var_name] = qr.paginate(kwargs["page"], 5)
     return render_template(template_name, **kwargs)
 
 
-@app.route('/list/')
+@app.route("/list/")
 def userstory_list():
-    return object_list('list.html', UserStory.select(), 'story_list')
+    return object_list("list.html", UserStory.select(), "story_list")
 
 
-@app.route('/story/', methods=['GET', 'POST'])
+@app.route("/story/", methods=["GET", "POST"])
 def new_userstory():
-    fields = ["story_title", "user_story", "acceptance_criteria", "business_value", "estimation", "status"]
-    kwargs = {"_data": {"submit": 'Create', "title": "Add new"}}
-    if request.method == "POST" and all([request.form[i] for i in fields]):
-        data = {i: request.form[i] for i in fields}
+    if request.method == "POST" and all(request.form.to_dict().values()):
         if request.args.get("story_id"):
-            UserStory.update(**data).where(UserStory.id == request.args.get("story_id")).execute()
-            flash('User Story updated!')
+            UserStory.update(**request.form.to_dict()).where(UserStory.id == request.args.get("story_id")).execute()
+            flash("User Story updated!")
         else:
-            user_story = UserStory.create(**data)
-            flash('New User Story added!')
-        return redirect(url_for('homepage'))
-    elif request.args.get('story_id'):
-        kwargs.update(UserStory.get(UserStory.id == request.args.get("story_id")).__dict__)
-        kwargs['_data']['submit'], kwargs['_data']["title"] = "Update", "Update"
-    return render_template('form.html', **kwargs['_data'])
+            user_story = UserStory.create(**request.form.to_dict())
+            flash("User Story created!")
+        return redirect(url_for("homepage"))
+    kwargs = {"submit": "Create", "story": ""}
+    if request.args.get("story_id"):
+        kwargs["story"] = UserStory.get(UserStory.id == request.args.get("story_id"))
+        kwargs["submit"] = "Update"
+    return render_template("form.html", **kwargs)
 
 
-@app.route('/delete/', methods=['GET'])
+@app.route("/delete/", methods=["GET"])
 def delete_userstory():
     UserStory.delete().where(UserStory.id == request.args.get("story_id")).execute()
-    flash('User Story deleted!')
-    return redirect(url_for('homepage'))
+    flash("User Story deleted!")
+    return redirect(url_for("homepage"))
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     if not os.path.isfile("homework.db"):
         create_tables()
     app.run()
